bc73b34b28d28443d3731d2c41ad5d4a1711e95d
siv <subhashpadala18@gmail.com>
2023-12-07 17:32:16 +0530

    change Chapter-2

--- Chapter-2/agent.py
+++ Chapter-2/agent.py
@@ -1,36 +1,61 @@
-# Agent needs to interact with the environment
-# keep note of it's q estimate, which is the estimate of mean of the rewards
-# take actions using this, and explore if necessary, and exploit
-import random
 import numpy as np
+import random
+from Bandit import Bandit
 
 class Agent():
-    def __init__(self, epsilon = 0.01, numActions=10):
-        self.epsilon = epsilon
-        self.numActions = numActions
-        self.q_estimate = [0 for i in range(1, numActions+1)]
-        self.previousAction = 0
-        self.actionTakenCount = [0 for i in range(1, numActions+1)]
+    """It helps to create an agent for stationary, non-stationary environments, can handle exploration and exploitation tradeoff for a k-armed bandit problem.
+    """
+    def __init__(self, numActions=10, epsilon=0.01, alpha=0):
+        """_summary_
 
-    def chooseAction(self, reward):
-        if np.random.rand() < self.epsilon or reward==0:
-            # exploration
-            # print("Explore")
-            return np.random.randint(1, self.numActions+1)
-        else:
-            # exploration
-            if self.previousAction!=0:
-                # updating the previousAction's q estimate using the reward received.
-                self.q_estimate[self.previousAction-1] = (self.q_estimate[self.previousAction-1]*self.actionTakenCount[self.previousAction-1]+reward)/(self.actionTakenCount[self.previousAction-1]+1.0)
-                self.actionTakenCount[self.previousAction-1] = self.actionTakenCount[self.previousAction-1]+1
-            self.previousAction = self.q_estimate.index(max(self.q_estimate)) + 1
-            # print("previous action is ", self.previousAction)
+        Args:
+            numActions (int, optional): The number of actions available to take in the environment. Defaults to 10.
+            epsilon (float, optional): The exploration probability. Defaults to 0.01.
+            alpha (float, optional): Whether the distribution of the reward signals in the environment is stationary or non-stationary. Defaults to 0, ie, stationary distribution uses exponent, else constant step size.
+        """
+        # Q_n+1 = R_n+1 + stepsize (R_n-Qn)
+        self.numActions = numActions
+        self.epsilon = epsilon
+        self.estimates = {i: 0 for i in range(1, numActions+1)}
+        self.actionCount = {i: 0 for i in range(1, numActions+1)}
+        self.previousAction = 0
+        self.alpha = alpha
+        return
+
+    def chooseAction(self):
+        """chooseAction according to the agent settings.
+
+        Returns:
+            int: action to take to maximize the expected reward.
+        """
+        if np.random.rand() < self.epsilon:
+            # Exploration
+            self.previousAction = np.random.randint(1, self.numActions+1)
+            self.actionCount[self.previousAction]+=1
             return self.previousAction
-        
-    def learn(self, bandit, nplays=1000):
-        reward = 0
-        average_reward = 0
-        for i in range(nplays):
-            reward = bandit.makeAction((self.chooseAction(reward)))
-            average_reward += reward
-        return average_reward/nplays
+        else:
+            # Exploitation
+            self.previousAction = max(self.estimates, key=lambda x: self.estimates[x])
+            self.actionCount[self.previousAction]+=1
+            return self.previousAction
+    
+    def update(self, reward):
+        """Used to update the estimates
+
+        Args:
+            reward (float): The reward recieved for the action taken to update the estimates.
+        """
+        if self.alpha:
+            self.estimates[self.previousAction] = self.estimates[self.previousAction] + (reward - self.estimates[self.previousAction])/self.alpha
+        else:
+            self.estimates[self.previousAction] = self.estimates[self.previousAction] + 1/self.actionCount[self.previousAction] (reward - self.estimates[self.previousAction])
+        return
+            
+
+if __name__ == "__main__":
+    agent = Agent(10, 0, 1)
+    bandit = Bandit()
+    for i in range(10):
+        print(agent.chooseAction(), ":", agent.estimates)
+        agent.update(1)
+
--- Chapter-2/bandit.py
+++ Chapter-2/bandit.py
@@ -1,22 +1,35 @@
 import numpy as np
+import random
 
-class kBandit():
-    def __init__(self, k=10, std=1):
+class Bandit():
+    def __init__(self, k=10, std_of_all_action_reward_distribution=1, std_random_walk=0):
+        """k-armed bandit environment, with expected reward for each action is sampled from normal distribution (0, 1), and Reward during each action is sampled from stationary or non-stationary distribution with inital mean 0, and std deviation given.
+        For non-stationary distribution randomwalks are taken, ie, mean <- mean + N(mean, std_random_walk)
+
+        Args:
+            k (int, optional): The number of arms for the bandit. Defaults to 10.
+            std_of_all_action_reward_distribution (int, optional): standard deviation of all actions stationary reward distribution whose mean is sampled from normal distribution . Defaults to 1.
+            std_random_walk (int, optional): For non-statinary distribution it is non-zero. Defaults to 0.
         """
-        k is the number of arms of the bandit,
-        std of each action's associated reward's stationary prob distribution
-        """
+        self.std_of_all_action_reward_distributions = std_of_all_action_reward_distribution
+        self.expected_rewards = {i: np.random.normal(0, 1) for i in range(1, k+1)}
+        self.std_random_walk = std_random_walk
         self.k = k
-        self.std = std
-        self.actions = [i for i in range(1, k+1)]
-        self.mean_rewards = [np.random.normal(0, 1) for i in range(1, k+1)]
-        print(f"mean_rewards is {self.mean_rewards}")
-    def makeAction(self, action):
-        if action == 0:
-            return 0
-        # print(action)
-        assert(action <= self.k and action >= 0)
-        return np.random.normal(self.mean_rewards[action-1], self.std)
-
-
     
+    def makeMove(self, action):
+        reward = np.random.normal(self.expected_rewards[action], self.std_of_all_action_reward_distributions)
+        # mean changes for non-stationary distribution alone
+        self.expected_rewards[action] += np.random.normal(0, self.std_random_walk)
+        return reward
+    
+    def reset(self):
+        self.expected_rewards = {i: np.random.normal(0, 1) for i in range(1, self.k+1)}
+        return
+    
+if __name__=="__main__":
+    # code for testing
+    bandit = Bandit(10, 1, 0.01)
+    print(bandit.expected_rewards)
+    bandit.makeMove(1)
+    print(bandit.expected_rewards)
+        
